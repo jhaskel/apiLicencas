84276986282236f8d841e9ed4b9dc05a6976003c
Joao Haskel <johaskel@gmail.com>
2020-12-21 18:11:41 -0300

    novos

---
+++ src/main/java/com/doisbitsw/licencas/api/email/service/EmailService.java
@@ -0,0 +1,54 @@
+package com.doisbitsw.licencas.api.email.service;
+
+import com.sun.org.apache.xml.internal.serialize.XHTMLSerializer;
+import lombok.var;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import com.doisbitsw.licencas.api.email.domain.User;
+
+import javax.mail.MessagingException;
+
+
+@Service
+public class EmailService {
+
+
+
+	private final JavaMailSender javaMailSender;
+
+	public EmailService(JavaMailSender javaMailSender) {
+		this.javaMailSender = javaMailSender;
+	}
+
+	public void sendMail(User user) throws MessagingException {
+		javax.mail.internet.MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
+		helper.setSubject("Welcome " + user.getFirstName());
+
+		String html = "<!doctype html>\n" +
+				"<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\"\n" +
+				"      xmlns:th=\"http://www.thymeleaf.org\">\n" +
+				"<head>\n" +
+				"    <meta charset=\"UTF-8\">\n" +
+				"    <meta name=\"viewport\"\n" +
+				"          content=\"width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0\">\n" +
+				"    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\n" +
+				"    <title>Email</title>\n" +
+				"</head>\n" +
+				"<body>\n" +
+				"<div>Welcome <b>" + user.getFirstName() + "</b></div>\n" +
+				"\n" +
+				"<div> Your username is <b>" + user.getLastName() + "</b></div>\n" +
+				"</body>\n" +
+				"</html>\n";
+		helper.setText(html, true);
+		helper.setTo(user.getEmailAddress());
+		javaMailSender.send(mimeMessage);
+	}
+}
