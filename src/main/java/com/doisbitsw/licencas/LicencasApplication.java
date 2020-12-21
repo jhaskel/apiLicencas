1c3db321acce3f909fc0080b9ceb89cefa782406
Joao Haskel <johaskel@gmail.com>
2020-12-21 18:10:56 -0300

    email config

--- src/main/java/com/doisbitsw/licencas/LicencasApplication.java
+++ src/main/java/com/doisbitsw/licencas/LicencasApplication.java
@@ -3,8 +3,12 @@ package com.doisbitsw.licencas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+
+
 public class LicencasApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/doisbitsw/licencas/api/empreendedor/EmpreendedorController.java
+++ src/main/java/com/doisbitsw/licencas/api/empreendedor/EmpreendedorController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/empreendedor")
+
 public class EmpreendedorController {
     @Autowired
     private EmpreendedorService service;
