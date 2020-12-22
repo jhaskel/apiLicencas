0e287516e4cedfd86c4884f1b313a90e0e1f0588
Joao Haskel <johaskel@gmail.com>
2020-12-22 17:37:56 -0300

    empreendedor_admin2

--- src/main/java/com/doisbitsw/licencas/api/infra/security/SecurityConfig.java
+++ src/main/java/com/doisbitsw/licencas/api/infra/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/email","/api/v1/login","/api/v1/usuarios","/sendmail/{email}","/api/v1/email/{id}","/empreendedor")
+                .antMatchers(HttpMethod.GET, "/email","/api/v1/login","/api/v1/usuarios","/sendmail/{email}","/api/v1/email/{id}","/api/v1/empreendedor")
                 .permitAll()
                 .antMatchers(HttpMethod.POST, "/api/v1/usuarios","/email")
                 .permitAll()
