3195f3fc8c9912a39611063ab76958b4cfdca79f
Joao Haskel <johaskel@gmail.com>
2020-12-29 09:35:15 -0300

    financeiro query

--- src/main/java/com/doisbitsw/licencas/api/financeiro/FinanceiroRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/financeiro/FinanceiroRepository.java
@@ -8,11 +8,10 @@ import java.util.List;
 public interface FinanceiroRepository extends JpaRepository<Financeiro, Long> {
 
 
-    @Query(value = "SELECT fin.*, dor.nome as nome,dor.cpf as cpf, dor.celular as celular,mento.logradouro as logradouro \n" +
-            ",mento.numero as numero,mento.bairro as bairro, pro.iscertidao as certidao,\n" +
-            "pro.caracterizacao as caracterixacao ,pro.quantidade as quantidade, pro.atividade as atividade, \n" +
-            "fas.nome as fase\n" +
-            " FROM financeiro fin\n" +
+    @Query(value = "SELECT fin.*, dor.nome as nome,dor.cpf as cpf, dor.celular as celular,mento.logradouro as logradouro,\n" +
+            "            mento.numero as numero,mento.bairro as bairro, pro.iscertidao as certidao, pro.caracterizacao as caracterizacao, \n" +
+            "pro.quantidade as quantidade, pro.atividade as atividade, fas.nome as fase \n" +
+            "FROM financeiro fin          \n" +
             "INNER JOIN processo pro ON pro.code = fin.code_processo  \n" +
             "INNER JOIN empreendimento mento ON mento.id = fin.empreendimento\n" +
             "INNER JOIN empreendedor dor ON dor.id = mento.empreendedor\n" +
