b23bdcca5e69b4bd2388d16da4a8d43942a36604
Joao Haskel <johaskel@gmail.com>
2020-12-29 09:14:55 -0300

    financeiro query

--- src/main/java/com/doisbitsw/licencas/api/financeiro/FinanceiroRepository.java
+++ src/main/java/com/doisbitsw/licencas/api/financeiro/FinanceiroRepository.java
@@ -8,10 +8,9 @@ import java.util.List;
 public interface FinanceiroRepository extends JpaRepository<Financeiro, Long> {
 
 
-    @Query(value = "SELECT fin.*, dor.nome as nomedor\n" +
+    @Query(value = "SELECT fin.*, mento.nome as nomedor\n" +
             " FROM financeiro fin\n" +
             "INNER JOIN empreendimento mento ON mento.id = fin.empreendimento\n" +
-            "INNER JOIN empreendedor dor ON dor.id = mento.empreendedor\n" +
             "WHERE fin.code_processo = :codeProcesso", nativeQuery = true)
     List<Financeiro> findByCodeProcesso(String codeProcesso);
 
